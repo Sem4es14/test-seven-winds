c9e499f4c34947e0b106bb1d783c08da7671acb7
Semyon <ily-senin@yandex.ru>
2023-09-19 18:47:17 +0300

    delete old

--- src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
@@ -35,32 +35,3 @@ class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
     }
 }
 
-/*
-package mobi.sevenwinds.app.budget
-
-import mobi.sevenwinds.app.budget.dto.enums.BudgetType
-import mobi.sevenwinds.app.budget.dto.responses.BudgetRecordResponse
-import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.IntIdTable
-
-object BudgetTable : IntIdTable("budget") {
-    val year = integer("year")
-    val month = integer("month")
-    val amount = integer("amount")
-    val type = enumerationByName("type", 100, BudgetType::class)
-}
-
-class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<BudgetEntity>(BudgetTable)
-
-    var year by BudgetTable.year
-    var month by BudgetTable.month
-    var amount by BudgetTable.amount
-    var type by BudgetTable.type
-
-    fun toResponse(): BudgetRecordResponse {
-        return BudgetRecordResponse(year, month, amount, type)
-    }
-}*/
